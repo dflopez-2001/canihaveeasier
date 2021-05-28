aafc78398e3bd149ba7b3fd28ddb9eeb8273e9b4
dflopez-2001 <83239928+dflopez-2001@users.noreply.github.com>
2021-05-27 20:24:29 -0700

    Add files via upload

---
+++ particles.js
@@ -0,0 +1,38 @@
+class Particle {
+    constructor(x, y) {
+        var options = {
+            restitution: 0,
+            friction: 0,
+            density: 0.6,
+        }
+        this.r = 10;
+        this.body = Bodies.circle(x, y, this.r, options);
+        this.color=color(random(0, 255), random(0,255), random(0, 255));
+        World.add(world, this.body);
+        
+        World.add(world, this.body);
+    }
+    display() {
+
+        var pos = this.body.position;
+        var angle = this.body.angle;
+
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        imageMode(CENTER);
+        noStroke();
+        fill("white");
+        ellipseMode(RADIUS);
+        ellipse(0,0,this.r,this.r);
+        pop();
+
+
+     
+
+
+
+
+    }
+
+};
